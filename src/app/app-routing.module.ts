3d8f9b3c6990e6b6b9847ec7dbf3761348d805ae
masaya.toda <masaya.toda@nextbeat.net>
2022-11-22 17:05:46 +0900

    Todoデータ表示画面モックアップ作成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoAddComponent } from './todo/todo-add/todo-add.component';
+import { TodoEditComponent } from './todo/todo-edit/todo-edit.component';
+
+const routes: Routes = [
+  { path: '', component: TodoListComponent },
+  { path: 'todo', component: TodoListComponent },
+  { path: 'todo/add', component: TodoAddComponent },
+  { path: 'todo/show/:id', component: TodoEditComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,45 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoEditComponent } from './todo/todo-edit/todo-edit.component';
+import { TodoAddComponent } from './todo/todo-add/todo-add.component';
+import { CardComponent } from './todo/component/card/card.component';
+import { FormComponent } from './todo/component/form/form.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoEditComponent,
+    TodoAddComponent,
+    CardComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatChipsModule,
+    MatBadgeModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
