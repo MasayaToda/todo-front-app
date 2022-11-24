57e4fa711e2ed1533558c2ee7dd991300539114a
masaya.toda <masaya.toda@nextbeat.net>
2022-11-24 12:56:45 +0900

    参照画面実装、ページ遷移アニメーションを実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
-
+import { RouterOutlet, ChildrenOutletContexts } from '@angular/router';
+import { slideInAnimation} from './animation'
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+  <mat-toolbar color="primary">
+    <span>My Application</span>
+    <span class="spacer" > </span> 
+    <a mat-flat-button color="primary" routerLink="todo">Todo</a>
+  </mat-toolbar>
+    <div [@routeAnimations]="prepareRoute(outlet)">
+      <router-outlet #outlet="outlet"></router-outlet>
+    </div>
+  `,
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    slideInAnimation
+  ]
 })
 export class AppComponent {
   title = 'todo-front-app';
+  constructor(private contexts: ChildrenOutletContexts) {}
+  prepareRoute(outlet: RouterOutlet) {
+    return (
+      outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation']
+    );
+  }
 }
--- src/app/todo/component/form/form.component.ts
+++ src/app/todo/component/form/form.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup,Validators } from '@angular/forms';
-
+import { Event, NavigationEnd, ActivatedRoute, RouterEvent, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { TodoService } from '../../service/todo.service';
 @Component({
   selector: 'todo-form',
   templateUrl: './form.component.html',
@@ -9,10 +11,50 @@ import { FormControl, FormGroup,Validators } from '@angular/forms';
 export class FormComponent {
   todoForm: FormGroup;
   categories: any;
-  constructor() {
+  isAddPage:boolean = false;
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private todoService: TodoService
+  ) {
+    // Form初期化
     this.todoForm = new FormGroup({
       title:    new FormControl('', Validators.required),
-      category: new FormControl('', Validators.required)
+      category: new FormControl('', Validators.required),
+      body: new FormControl('', Validators.required),
+      state: new FormControl('', Validators.required),
     });
+  }
+  
+  ngOnInit(): void {
+    this.route.url.subscribe(url => {
+      // URLから登録ページか編集ページか判断 ※決め打ち
+      this.isAddPage = url[1].path !== "add"
+      if(this.isAddPage){
+        // 編集ページの場合
+        this.route.params.subscribe(params => {
+          // データを取得
+          this.getTodo(Number(params['id']))
+        });
+      }
+    });
+  }
+
+  getTodo(id:number) {
+    return this.todoService.getTodo(id).subscribe((data)=>{
+      console.log(data)
+      this.todoForm = new FormGroup({
+        title:    new FormControl(data.title, Validators.required),
+        category: new FormControl(data.categoryId, Validators.required),
+        body: new FormControl(data.body, Validators.required),
+        state: new FormControl(data.state, Validators.required),
+      });
+    })
+  }
+  onAddButtonClick(){
+
+  }
+  onUpdateButtonClick(){
+    
   }
 }
