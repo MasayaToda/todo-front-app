cd599873cbcdd8544f772dcb6c7cc6e8de9488da
masaya.toda <masaya.toda@nextbeat.net>
2022-11-28 17:43:40 +0900

    デザインを修正

--- src/app/animation.ts
+++ src/app/animation.ts
@@ -45,4 +45,22 @@ import {
       ]),
       query(':enter', animateChild()),
     ]),
+    transition('Page3 => Page3, Page2 => Page2, Page1 => Page1', [
+      style({ position: 'relative' }),
+      query(':enter, :leave', [
+        style({
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+        }),
+      ]),
+      query(':enter', [style({ bottom: '-100%' })]),
+      query(':leave', animateChild()),
+      group([
+        query(':leave', [animate('300ms ease-out', style({ bottom: '100%' }))]),
+        query(':enter', [animate('300ms ease-out', style({ bottom: '0%' }))]),
+      ]),
+      query(':enter', animateChild()),
+    ]),
   ]);
--- src/app/category/component/form/form.component.spec.ts
+++ src/app/category/component/form/form.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FormComponent } from './form.component';
+import { CategoryFormComponent } from './form.component';
 
-describe('FormComponent', () => {
-  let component: FormComponent;
-  let fixture: ComponentFixture<FormComponent>;
+describe('CategoryFormComponent', () => {
+  let component: CategoryFormComponent;
+  let fixture: ComponentFixture<CategoryFormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FormComponent ]
+      declarations: [ CategoryFormComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(FormComponent);
+    fixture = TestBed.createComponent(CategoryFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
